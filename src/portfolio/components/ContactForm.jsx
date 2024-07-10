a2ebca2edb68772db9794358c3f0d665e01d4e54
Harold Revueltas Gómez <105690550+HRevueltas@users.noreply.github.com>
2024-07-10 00:05:24 -0500

    contact form

--- src/portfolio/components/ContactForm.jsx
+++ src/portfolio/components/ContactForm.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import { Box, Flex } from '@radix-ui/themes';
+import { Box, Flex, Text } from '@radix-ui/themes';
+import { MobileIcon } from '@radix-ui/react-icons';
+import styles from './ContactForm.module.css';
 
 export const ContactForm = () => {
-    return (
-        <Flex direction={{ initial: 'column', lg: 'row' }} align="center" justify={{ initial: 'center', lg: 'between' }} p="4">
-
-            <Box  style={{ width: '100%', height: '200px', backgroundColor: 'blue'  }} mb={'-8'}>
-
-            </Box>
-
-            <Box as="form" width={{ initial: '100%', lg: '50%' }} p="4" backgroundColor="lightgray" borderRadius="8px">
-                <Box mb="3">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" style={{ width: '100%', padding: '8px', borderRadius: '4px' }} />
-                </Box>
-
-                <Box mb="3">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" style={{ width: '100%', padding: '8px', borderRadius: '4px' }} />
-                </Box>
-
-                <Box mb="3">
-                    <label htmlFor="message">Message:</label>
-                    <textarea id="message" name="message" rows="4" style={{ width: '100%', padding: '8px', borderRadius: '4px' }} />
-                </Box>
-
-                <Box>
-                    <button type="submit" style={{ padding: '10px 20px', borderRadius: '4px', backgroundColor: '#007BFF', color: 'white' }}>
-                        Submit
-                    </button>
-                </Box>
-            </Box>
-        </Flex>
-    );
+  return (
+    <Flex className={styles.flexContainer} direction={{ initial: 'column', lg: 'column' }} align="center" justify={{ initial: 'center', lg: 'between' }}>
+      <Box className={styles.headerContainer}>
+        <Box className={styles.iconTextContainer}>
+          <MobileIcon className={styles.icon} />
+          <Text className={styles.iconText}>
+            Get in touch
+          </Text>
+        </Box>
+        <Text className={styles.headerText}>
+          CONTACT
+        </Text>
+      </Box>
+      <hr className={styles.separator} />
+      <Box className={styles.formContainer}>
+        <Box className={styles.sidebar}>
+          caja1
+        </Box>
+        <form className={styles.form}>
+          <input type="text" name="name" id="name" placeholder="Name" className={styles.input} />
+          <input type="email" name="email" id="email" placeholder="Email" className={styles.input} />
+          <textarea name="message" id="message" placeholder="Message" rows="4" className={styles.textarea} />
+          <button type="submit" className={styles.button}>
+            Send
+          </button>
+        </form>
+      </Box>
+    </Flex>
+  );
 };
